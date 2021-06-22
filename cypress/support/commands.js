ef1d837a64a06bf0e0990c2aefe3641fbab77f62
flaviafreitas <flavia.moreira.freitas@gmail.com>
2021-06-22 15:51:41 -0300

    commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,45 +1,20 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
 //Commands Login
 Cypress.Commands.add("login", dados => { 
   cy.visit("http://automationpractice.com/index.php?controller=authentication&back=my-account");
-  cy.get("#email").type(dados.Email);
-  cy.get("#passwd").type(dados.Senha);
+  cy.get("#email").type(dados.email);
+  cy.get("#passwd").type(dados.senha);
   cy.get("#SubmitLogin").click();
  });
 
- Cypress.Commands.add("alertaLoginValido", valido => { 
+ Cypress.Commands.add("alertaLoginValido", () => { 
   cy.get(".page-heading").should("exist", "MY ACCOUNT");
  });
 
- Cypress.Commands.add("alertaLoginInvalido", invalido => { 
+ Cypress.Commands.add("alertaLoginInvalido", () => { 
   cy.get(".alert.alert-danger").should("exist");
  });
 
+ //Commands Compra
  Cypress.Commands.add("compra", () => { 
   cy.get("#header_logo").click();
   cy.get('.button.ajax_add_to_cart_button.btn.btn-default').first().click()
@@ -51,5 +26,3 @@ Cypress.Commands.add("login", dados => {
   cy.get('.bankwire').click()
   cy.xpath('//*[@id="cart_navigation"]/button').click()
  });
-
-//Commands
